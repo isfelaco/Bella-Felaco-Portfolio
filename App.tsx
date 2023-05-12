6f72827d2ecf6fce58dd9ebc42dd3a6215f8f4ea
isfelaco <isfelaco@gmail.com>
2023-05-12 14:39:26 -0400

    Updates

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { About } from "./pages/About";
+import { Experience } from "./pages/Experience";
+import { Projects } from "./pages/Projects";
+import "./css/App.css";
+
+export const pages: { [id: string]: ReactElement } = {
+	"/": <HomePage />,
+	"/about": <About />,
+	"/experience": <Experience />,
+	"/projects": <Projects />,
+};
+
+export function App() {
+	return (
+		<div className="App">
+			<BrowserRouter basename="Bella-Felaco-Porfolio">
+				<Routes>
+					{Object.entries(pages).map(([route, element], i) => (
+						<Route path={route} element={element} key={i} />
+					))}
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+}
