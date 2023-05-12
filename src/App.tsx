77f0646df1cfb5fdf5201af5930a86d0778e161f
isfelaco <isfelaco@gmail.com>
2023-05-12 17:24:05 -0400

    Removing test text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ export const pages: { [id: string]: ReactElement } = {
 export function App() {
 	return (
 		<div className="App">
-			hello
 			<Routes>
 				{Object.entries(pages).map(([route, element], i) => (
 					<Route path={route} element={element} key={i} />
