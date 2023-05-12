65250872ea3cb6d167dcefadb08d19ce2c4f7454
isfelaco <isfelaco@gmail.com>
2023-05-12 17:46:00 -0400

    Test 2

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<HashRouter basename={process.env.PUBLIC_URL}>
+			hi
 			<App />
 		</HashRouter>
 	</React.StrictMode>
